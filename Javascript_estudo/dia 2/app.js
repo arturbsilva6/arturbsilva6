1c847b958d6007c900dfc784029e601d2a54269a
arturbsilva6 <arturbsilva6@gmail.com>
2025-04-08 11:45:42 -0300

    adicionando a pasta estudos

---
+++ Javascript_estudo/dia 2/app.js
@@ -0,0 +1,36 @@
+/*let nome = "João";
+const idade = 30;
+
+console.log("Nome:", nome);
+console.log("idade",idade);
+
+nome = "Carlos"; //devido o elemento a ser usado (let) podemos trocar seu valor
+console.log ("Nome atualizado:", nome);*/
+
+/*Exercicios de const e let junto com interpolação e concatenação
+
+
+let NomeVerdadeiro = "Artur";
+const NomeSuperHeroi = "Homem-aranha";
+
+console.log("Meu nome verdadeiro é " + NomeVerdadeiro); // sinal de + significa concatenação
+console.log(`Mais meu nome de Super-heroi é ${NomeSuperHeroi}`); /*usa crase `` significa intepolação, servem para a mesma coisa,
+                                                                   só que intepolação deixa o codigo mais limpo e melhor de se ver.
+                                                                   O $ significa que tudo tem o JS ler dentro do {} vai ser executado 
+                                                                   o que estiver ali*/
+
+
+
+/*let MeuNome = "Artur";
+const MinhaIdade = 18;
+let ProfissãoDosSonohos = "Engenheiro de sofware";
+
+
+console.log(`Olá, meu nome é ${MeuNome}, tenho ${MinhaIdade} e quero ser ${ProfissãoDosSonohos}`); */
+
+const meuNomeHeroi = "Artur";
+const meuSuperPoder = "Voar";
+const minhaFraqueza =  "pó";
+
+
+console.log(`Eu sou o Incrivel ${meuNomeHeroi}! Tenho o poder de ${meuSuperPoder}, mais minha fraqueza é ${minhaFraqueza}`);
---
+++ Javascript_estudo/dia1/pratica.js
@@ -0,0 +1,13 @@
+var botao = document.getElementById('salvar');
+let contador = 0
+
+
+botao.addEventListener('click', function(){
+
+    contador++; // aumenta o  valor da contagem
+    console.log ("Numero de cliques:", contador) //atualiza o numero no html
+
+});
+
+
+
