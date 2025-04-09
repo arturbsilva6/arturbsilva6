2697ed7ee372410cf81a9685c4c40dbf36d38ee1
arturbsilva6 <arturbsilva6@gmail.com>
2025-04-09 16:36:16 -0300

    ultima atualização

--- Javascript_estudo/dia 3/app.js
+++ Javascript_estudo/dia 3/app.js
@@ -17,8 +17,9 @@ else{console.log("Entrada negada")}
 */
 
 
-let nota = prompt("Digite sua nota")
+let nota = Number(prompt("Digite sua nota"))// a tag prompt sempre convete em texto (string),sempre conveter para number nestes casos
 const nomeAluno = prompt("Digite o nome do aluno");
+
 
 if (nota >= 7){
     console.log(`${nomeAluno} foi aprovado(a)!`)
