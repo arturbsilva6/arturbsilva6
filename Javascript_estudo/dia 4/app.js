f635f7b1b6d5c28e8d6df85cff071872eb6468e3
arturbsilva6 <arturbsilva6@gmail.com>
2025-04-22 16:54:48 -0300

    atualização

--- Javascript_estudo/dia 4/app.js
+++ Javascript_estudo/dia 4/app.js
@@ -8,10 +8,22 @@ console.log(meusFilmesFavoritos.length);
 meusFilmesFavoritos.push("Vingadores");
 meusFilmesFavoritos.pop()*/
 
-let meusJogosFavoritos = ["Need for speed", "Forzan 2", "Elder Ring"];
+/*let meusJogosFavoritos = ["Need for speed", "Forzan 2", "Elder Ring"];
 
 for( let i = 0; i < meusJogosFavoritos.length; i++){
     console.log(`Jogo 1 ${i+1}: ${meusJogosFavoritos[i]}`)
 }
 
-console.log(meusJogosFavoritos.lenght);
+console.log(`Voce tem ${meusJogosFavoritos.length} jogos na sua lista'`);*/
+
+
+let minhasComidasFavoritas = ["Lasanha", "Nhoque", "Pizza"];
+
+function mostrarComidas(comidas){
+    for (let i=0; i < comidas.length; i++){
+        console.log(`Comida ${i+1}: ${comidas[i]}`)
+    }
+    console.log(`Voce tem no total ${comidas.length} comidas favoritas`)
+}
+
+mostrarComidas(minhasComidasFavoritas);
