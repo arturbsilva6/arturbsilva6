8511efcb34aeed45d873eb267f11d6a8f9180eb1
arturbsilva6 <arturbsilva6@gmail.com>
2025-04-22 16:17:39 -0300

    acionando dia 4

--- Javascript_estudo/dia 4/app.js
+++ Javascript_estudo/dia 4/app.js
@@ -1,53 +1,9 @@
-//Função
+meusFilmesFavoritos = ["Rei leão", "Homem aranha", "Circulo de fogo"];
 
 
-/*1. Exercicio
+console.log(meusFilmesFavoritos[0]);
+console.log(meusFilmesFavoritos[2]);
+console.log(meusFilmesFavoritos.length);
 
-function boasVindas(nome){
-    console.log(`Seja bem vindo(a), ${nome}`)
-}
-
-boasVindas("Artur");*/
-
-/*2.exercicio
-function apresentarPessoa(nome,idade){
-    console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`)
-}
-
-apresentarPessoa("Artur", 18);*/
-
-
-/*3. exercicio 
-function criarMensagem(nome, profissao){
-
-   return`Olá, meu nome é ${nome} e sou ${profissao}`
-
-}
-
-let mensagem = criarMensagem("Artur", "Engheiro de sofware")
-
-console.log(mensagem)*/
-/* Exercicio 4 IMC
-function calcularImc(peso, altura){
-    return  peso / (altura * altura)
-}
-
-let imc = calcularImc(70, 1.75);
-console.log (`Seu IMC é ${imc}`);*/
-
-
-function calcularDesconto( precoOriginal,porcentagemDesconto){
-
-    //VALIDAR SEO NUMERO ESTÁ ENTRE 0 A 100 PARA PORCENTAGEM
-
-    if (porcentagemDesconto < 0 || porcentagemDesconto > 100){
-        console.log("Porcentagem invalida! DIGITE UM VALOR ENTRE 0 A 100")
-        return
-    }
-
-    return precoOriginal - (precoOriginal * porcentagemDesconto/100)
-
-}
-
-let precoComDesconto = calcularDesconto(100,200);
-console.log(`O valor com desconto é R$ ${precoComDesconto}`);
+meusFilmesFavoritos.push("Vingadores");
+meusFilmesFavoritos.pop()
