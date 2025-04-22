5f387c4661425fade049de36245b06fb6ac7a922
arturbsilva6 <arturbsilva6@gmail.com>
2025-04-22 16:38:10 -0300

    atualizando dia 4

--- Javascript_estudo/dia 4/app.js
+++ Javascript_estudo/dia 4/app.js
@@ -1,4 +1,4 @@
-meusFilmesFavoritos = ["Rei leão", "Homem aranha", "Circulo de fogo"];
+/*let meusFilmesFavoritos = ["Rei leão", "Homem aranha", "Circulo de fogo"];
 
 
 console.log(meusFilmesFavoritos[0]);
@@ -6,4 +6,12 @@ console.log(meusFilmesFavoritos[2]);
 console.log(meusFilmesFavoritos.length);
 
 meusFilmesFavoritos.push("Vingadores");
-meusFilmesFavoritos.pop()
+meusFilmesFavoritos.pop()*/
+
+let meusJogosFavoritos = ["Need for speed", "Forzan 2", "Elder Ring"];
+
+for( let i = 0; i < meusJogosFavoritos.lenght; i++){
+    console.log(`Jogo 1 ${i+1}: ${meusJogosFavoritos[i]}`)
+}
+
+console.log(meusJogosFavoritos.lenght);
